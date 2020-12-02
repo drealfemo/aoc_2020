b17721a913cf0f7dbe6d6b90aedc13fe78c58af2
Gideon Adekunle <gideonf.adekunle@gamil.com>
2020-12-02 19:20:20 +0000

    comment for day one

--- day_one/day_one.py
+++ day_one/day_one.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with open(os.path.join(sys.path[0], "input.txt")) as f:
         input_list = map(int, f.readlines())
 
-    comb_num = 3
+    comb_num = 3  # change this value to 2 for the first part of the question
     res = [x for x in combinations(input_list, comb_num) if sum(x) == 2020]
     res = prod(res[0])
